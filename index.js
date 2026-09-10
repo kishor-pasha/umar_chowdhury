72e9a4f0d3c792fc56834e6238222c123a8ab1fe
Kishor Pasha <uc.310310@gmail.com>
2026-09-11 03:14:00 +0600

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarMenu = document.querySelector('.nav-list');
+const introButtons = document.querySelector('.buttons')
+const readMore = document.querySelectorAll(".read-more");
+
 
 navbarToggle.addEventListener('click', () => {
     navbarToggle.classList.toggle('active');
     navbarMenu.classList.toggle('active');
+    introButtons.classList.toggle('active');
+});
+
+readMore.forEach(button => {
+    button.addEventListener("click", function () {
+        const text = this.previousElementSibling;
+        const moreText = text.querySelector(".more-text");
+
+        moreText.classList.toggle("show");
+
+        this.textContent = moreText.classList.contains("show")
+            ? "Read Less"
+            : "Read More";
+    });
 });
